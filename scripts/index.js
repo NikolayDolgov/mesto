3ff2de05048fb06c60d7b2460d93d92a884ae521
Nikolay Dolgov <koly201020@yandex.ru>
2022-01-16 17:50:08 +0300

    Submit a project

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,17 @@
 // Открываем форму
 let buttonName = document.querySelector('.profile__button-name');
 function openForm() {
+  formSpaceholder (); // запускаем функцию для заполнения input of profile
   let popup = document.querySelector('.popup');
   popup.classList.add('popup_opened');
 }
 buttonName.addEventListener('click', openForm);
 
-
-
 // Функция для заполнения input of profile
 function formSpaceholder () {
   // Находим profile
   let profileElements = document.querySelector('.profile');
+
   // Поиск значений полей profile и их запись в переменные
   let nameInputOld = profileElements.querySelector('.profile__title-name');
   let jobInputOld = profileElements.querySelector('.profile__text');
@@ -19,42 +19,46 @@ function formSpaceholder () {
   let popupElements = document.querySelector('.popup');
   // Поиск значений полей popup и их запись в массив
   let inputAll = popupElements.querySelectorAll('.popup__input');
+
   // Перезапись атрибута placeholder в массиве и вывод в консоль
-  console.log(inputAll[0].placeholder = nameInputOld.textContent);
-  console.log(inputAll[1].placeholder = jobInputOld.textContent);
-  return inputAll;
+  inputAll[0].placeholder = nameInputOld.textContent;
+  inputAll[1].placeholder = jobInputOld.textContent;
+
+   // Перезапись атрибута value в массиве
+  nameInputOld.textContent = nameInputOld.textContent;
+  jobInputOld.value = jobInputOld.textContent;
+  
+  // Перезапись атрибута value в массиве
+  inputAll[0].value = '';
+  inputAll[1].value = '';
 }
-formSpaceholder ();
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__container');// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = formElement.querySelectorAll('.popup__input')[0];// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelectorAll('.popup__input')[1];// Воспользуйтесь инструментом .querySelector()
+
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                         // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.                                          
+                        // О том, как это делать, расскажем позже.   
+
   // Получите значение полей jobInput и nameInput из свойства value
-  nameInput = nameInput.value;
-  jobInput = jobInput.value;
-  // Проверяем наличие value
-  if (nameInput.value === '') {
-    inputAll = formSpaceholder ();
-    nameInput.textContent = inputAll[0].placeholder;
-    jobInput.textContent = inputAll[1].placeholder;
-  }
-  else {
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileElements = document.querySelector('.profile');
-    let newNameInput = profileElements.querySelector('.profile__title-name');
-    let jobNameInput = profileElements.querySelector('.profile__text');
-    // Вставьте новые значения с помощью textContent
-    console.log(newNameInput.textContent = nameInput);
-    console.log(jobNameInput.textContent = jobInput);
-  }
+  let nameInput = formElement.querySelectorAll('.popup__input')[0].value;
+  let jobInput = formElement.querySelectorAll('.popup__input')[1].value;
+  
+  // Выберите элементы, куда должны быть вставлены значения полей
+  let profileElements = document.querySelector('.profile');
+  let newNameInput = profileElements.querySelector('.profile__title-name');
+  let newJobInput = profileElements.querySelector('.profile__text');
+
+  // Вставьте новые значения с помощью textContent
+  newNameInput.textContent = nameInput;
+  newJobInput.textContent = jobInput;
+
+  // закрываем форму
+  let popup = document.querySelector('.popup');
+  popup.classList.toggle('popup_opened');
 }
 
 // Прикрепляем обработчик к форме:
@@ -68,34 +72,3 @@ function closeForm() {
   popup.classList.remove('popup_opened');
 }
 closePopup.addEventListener('click', closeForm);
-
-// Постановка/снятие "лайков"
-let buttonLike = document.querySelector('.elements');
-function LikeСheck() {
-
-  let like = document.querySelector('.elements');
-  /*if (like.classList.contains('element__button-view_active') === true) {
-    like.classList.toggle('element__button-view_active');
-    like.classList.toggle('element__button-view_disabled');
-  } 
-  else {
-    like.classList.toggle('element__button-view_active');
-    like.classList.toggle('element__button-view_disabled');
-  }*/
-
-    document.addEventListener('click', function (e) {
-      if (e.target.tagName === 'BUTTON') {
-        //e.target.remove();
-        let likeName = like.querySelectorAll('.element__button-view');
-        if (likeName.classList.contains('element__button-view_active') === true) {
-            likeName.classList.toggle('element__button-view_active');
-            likeName.classList.toggle('element__button-view_disabled');
-          } 
-          else {
-            likeName.classList.toggle('element__button-view_active');
-            likeName.classList.toggle('element__button-view_disabled');
-          }
-      }
-    });
-}
-buttonLike.addEventListener('click', LikeСheck);
