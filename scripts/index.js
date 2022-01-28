2aba0e47b4493d5dbbb02ebeea2d0e5a8b6052d3
Nikolay Dolgov <koly201020@yandex.ru>
2022-01-28 20:44:10 +0300

    feat: implement delete cards and add a pop-up

--- scripts/index.js
+++ scripts/index.js
@@ -7,9 +7,10 @@ let buttonAddCard = document.querySelector('.profile__add-button');
 let profileInfo = document.querySelector('.profile');
 let profileTitle = profileInfo.querySelector('.profile__title-name');
 let profileText = profileInfo.querySelector('.profile__text');
-// для elements
+// для elements // повторение переменных
 let buttonViewElements = document.querySelector('.elements');
-let buttonDeleteElements = buttonViewElements;
+let buttonDeleteElements = document.querySelector('.elements');
+let popupImgElement = document.querySelector('.elements');
 // переменные формы
 let popup = document.querySelector('.popup'); 
 let popupContainer = popup.querySelector('.popup__container'); 
@@ -86,12 +87,19 @@ function formAddCardSubmitHandler(evt) { // Функция добавления 
   closeForm ();
 }
 
-function closeForm() { // Функция закрытия формы
+function closeForm() { // Функция закрытия формы/img
   if (popupContainer.querySelector('.form').classList.contains('form_opend')) {
     popupContainer.querySelector('.form').classList.remove('form_opend');
   }
   if (popupContainer.querySelector('.form-card').classList.contains('form-card_opend')) {
     popupContainer.querySelector('.form-card').classList.remove('form-card_opend');
+  }
+  if (popupContainer.querySelector('.figure').classList.contains('figure_opened')) {
+    popupContainer.querySelector('.figure').classList.remove('figure_opened');
+    popupContainer.classList.remove('popup__container_hidden'); // изменяем попап
+    // удаляем figcaption
+    const figcaptionDel = popupContainer.querySelector('figcaption');
+    figcaptionDel.remove();
   }
   popup.classList.remove('popup_opened'); // удаляем класс
 }
@@ -123,7 +131,7 @@ popupContainer.querySelector('.form').addEventListener('submit', formSubmitHandl
 popupContainer.querySelector('.form-card').addEventListener('submit', formAddCardSubmitHandler);
 popupClose.addEventListener('click', closeForm);
 
-buttonViewElements.addEventListener('click', function (evt) { // функция оценки (лайк)
+buttonViewElements.addEventListener('click', function (evt) { // обработчик оценки (лайк)
   const eventTarget = evt.target;
 
   if(eventTarget.classList.contains('elements__button-view')) {
@@ -131,11 +139,30 @@ buttonViewElements.addEventListener('click', function (evt) { // функция 
   }
 });
 
-/*buttonDeleteElements.addEventListener('click', function (evt) { // функция удаления
+buttonDeleteElements.addEventListener('click', function (evt) { // обработчик удаления
   const eventTarget = evt.target;
 
   if(eventTarget.classList.contains('elements__button-delete')) {
-    const listItem = deleteButton.closest('.elements__element');
+    const listItem = eventTarget.closest('.elements__element');
     listItem.remove();
   }
-});*/
+});
+
+popupImgElement.addEventListener('click', function (evt) { // обработчик открытия img карточки
+  const eventTarget = evt.target;
+
+  if(eventTarget.classList.contains('elements__img')) {
+    // записываем переменные в img
+    popupContainer.querySelector('.figure__img').src = eventTarget.src;
+    popupContainer.querySelector('.figure__img').alt = eventTarget.alt;
+    popupContainer.querySelector('.figure').classList.add('figure_opened');
+    popupContainer.classList.add('popup__container_hidden'); // изменяем попап
+    openForm(); // открываем попап
+
+    // создание figcaption
+    const captionContainer = document.createElement('figcaption');
+    captionContainer.classList.add('figure__caption');
+    let fig = document.querySelector('.figure');
+    fig.insertAdjacentHTML('beforeend', `<figcaption class="figure__caption">${eventTarget.alt}</figcaption>`);
+    }
+});
